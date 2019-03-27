c2282d0369c3a4e8aa2756f89feb7f6bfdf87332
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-03-27 10:58:45 +0000

    update var

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_ssm_maintenance_window" "pre" {
   count    = "${var.weeks}"
   testval = "${var.weeks == "1" ? var.week : 0}"
-  name     = "pre_${var.type}_week-${count.index+1}_${var.day}_${var.hour}00_testval${testval}"
+  name     = "pre_${var.type}_week-${count.index+1}_${var.day}_${var.hour}00_testval${var.testval}"
   schedule = "cron(00 ${var.hour} ? 1/3 ${var.day}#${count.index+1} *)"
   duration = "${var.mw_duration}"
   cutoff   = "${var.mw_cutoff}"
