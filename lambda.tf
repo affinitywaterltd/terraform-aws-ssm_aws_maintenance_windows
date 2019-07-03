d2eec27cdbae804002bde5a950ac54220fd3b394
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-07-03 11:12:36 +0100

    Add lambda role

--- lambda.tf
+++ lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "auto_daily_mw_snapshot_cleanup" {
   function_name = "auto_daily_mw_snapshot_cleanup"
   filename      = "${path.module}/auto_daily_mw_snapshot_cleanup.zip"
 
-  role             = "${data.terraform_remote_state.core.lambda_report_role}" 
+  role             = "${aws_iam_role.lambda_snapshot_cleanup_role.arn}" 
   source_code_hash = "${base64sha256(file("${path.module}/auto_daily_mw_snapshot_cleanup.zip"))}"
   handler          = "auto_daily_mw_snapshot_cleanup.lambda_handler"
   runtime          = "python3.6"
@@ -34,7 +34,7 @@ resource "aws_lambda_permission" "allow_cloudwatch" {
 resource "aws_cloudwatch_event_target" "auto_daily_mw_snapshot_cleanup" {
   target_id = "auto_daily_mw_snapshot_cleanup"
   arn = "${aws_lambda_function.auto_daily_mw_snapshot_cleanup.arn}"
-  rule = "${var.cloudwatch_rule_name}"
+  rule = "${aws_cloudwatch_event_rule.schedule_daily.arn}"
 }
 
 resource "aws_cloudwatch_event_rule" "schedule_daily" {
@@ -42,4 +42,48 @@ resource "aws_cloudwatch_event_rule" "schedule_daily" {
   description = "Runs daily"
   schedule_expression = "cron(0 1 * * * *)"
   
+}
+
+# IAM role
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_snapshot_cleanup_role" {
+  name = "Lambda_Reporting"
+
+  assume_role_policy = "${data.aws_iam_policy_document.lambda_assume_role_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_readonly_policy_attach" {
+  role       = "${aws_iam_role.lambda_reporting_role.name}"
+  policy_arn = "${aws_iam_policy.ec2_cleanup_snapshot.arn}"
+}
+
+resource "aws_iam_policy" "ec2_cleanup_snapshot" {
+  name = "EC2Snapshot"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DeleteSnapshot",
+                "ec2:ModifySnapshotAttribute",
+                "ec2:Describe*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 }
